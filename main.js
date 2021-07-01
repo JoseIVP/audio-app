79939cd5695ad30fdd32b40843c003a7c052a08e
Jose Vergara <vergarajose365@gmail.com>
2021-07-01 11:28:27 -0400

    Added a BarChart component in replacement for BarPlot

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 import LinePlot from "./components/LinePlot.js";
-import BarPlot from "./components/BarPlot.js";
+// import BarPlot from "./components/BarPlot.js";
+import BarChart from 'https://cdn.jsdelivr.net/gh/joseivp/bar-chart@1.0/BarChart.js';
 import FourierComputer from "./computers/FourierComputer.js";
 import MLComputer from "./computers/MLComputer/MLComputer.js";
 import AnimationCicle from "./AnimationCicle.js";
 
 customElements.define("line-plot", LinePlot);
-customElements.define("bar-plot", BarPlot);
+customElements.define("bar-chart", BarChart);
 
 const linePlot = document.querySelector("line-plot");
-const barPlot = document.querySelector("bar-plot");
+const barChart = document.querySelector("bar-chart");
+barChart.plot({
+    size: 10,
+    gapFraction: 0.1,
+    padding: 30,
+    values: [7, 5, 2, 4, 8, 2, 1, 6, 3, 8],
+    maxValue: 8,
+    yLabels: [0, 2, 4, 6, 8],
+    yLabelsMapping: ['a', 'b', 'c', 'd', 'e'],
+    yLabelsGap: 10,
+    xLabels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    xLabelsGap: 5,
+    yLegend: 'Y Legend',
+    yLegendGap: 10,
+    xLegend: 'X Legend',
+    xLegendGap: 5,
+    title: 'Chart Title',
+    titleGap: 10,
+});
 
 const fourierComputer = new FourierComputer();
 const mlComputer = new MLComputer();
@@ -16,7 +35,7 @@ let selectedComputer = fourierComputer; // To compute pitch
 const animation = new AnimationCicle();
 
 animation.onUpdate = async () =>{
-    barPlot.update(fourierComputer.getFrequencyArray());
+    barChart.update(fourierComputer.getFrequencyArray());
     linePlot.update(await selectedComputer.getMaxFrequency());
 };
 
